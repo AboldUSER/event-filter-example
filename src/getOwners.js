6fcd5f33adf7d092ca41351b264dbe34a5322b77
bold <2985057+AboldUSER@users.noreply.github.com>
2022-08-09 11:21:35 -0700

    Fix getOwners script

--- src/getOwners.js
+++ src/getOwners.js
@@ -10,33 +10,38 @@ const run = async () => {
 
     const contractCreationBlock = 24155383; // I looked this up on a blockchain scanner - it is the block number that the contract was deployed. Having this just making filtering quicker.
     const contractAddress = process.env.CONTRACT_ADDRESS;
-    const alchemyProviderURL = `wss://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+    const alchemyWSProviderURL = `wss://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+    const alchemyHTTPSProviderURL = `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 
-    const wsProvider = new ethers.providers.WebSocketProvider(alchemyProviderURL)
+
+    const wsProvider = new ethers.providers.WebSocketProvider(alchemyWSProviderURL)
+    const httpProvider = new ethers.providers.JsonRpcProvider(alchemyHTTPSProviderURL)
 
     const nftContract = new ethers.Contract(contractAddress, abi.abi, wsProvider)
+    const httpsnftContract = new ethers.Contract(contractAddress, abi.abi, httpProvider)
 
     const filterQuery = await nftContract.queryFilter(nftContract.filters.Transfer(), contractCreationBlock)
 
     filterQuery.forEach(async tx => {
         // console.log(tx)
-        if(
+        if (
             tx.args[0] === ethers.constants.AddressZero &&
             (
                 tx.args[2].toString()[0] === "1" ||
                 tx.args[2].toString()[0] === "2"
             )
-            ) {
-                tokenIds.push(tx.args[2].toNumber());
+        ) {
+            tokenIds.push(tx.args[2].toNumber());
         }
-        tokenIds.forEach(async tokenId => {
-            const currentOwner = await nftContract.ownerOf(tokenId)
-            owners.push(currentOwner);
-        })
     })
-
     console.log(tokenIds);
-    console.log(currentOwners);
+    for (let i = 0; i < tokenIds.length; i++) {
+        const currentOwner = await nftContract.ownerOf(tokenIds[i])
+        currentOwners.push(currentOwner);
+    }
+
+    console.log(tokenIds)
+    console.log(currentOwners)
 
     wsProvider.websocket.close()
 }
